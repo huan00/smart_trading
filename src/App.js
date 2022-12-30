b5a8f2ebb18c9ec64f74638e9d4edfdaf0f0bd30
huan00 <96999144+huan00@users.noreply.github.com>
2022-12-30 15:18:41 -0500

    complete navbar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+import Navbar from './components/navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import './navbar.css'
+import { CgMenuMotion, CgMenu } from 'react-icons/cg'
+import logo from '../assets/logo.svg'
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#home">Home</a>
+    </p>
+    <p>
+      <a href="#wst">What is Smart Trading?</a>
+    </p>
+    <p>
+      <a href="#opportunities">Opportunities</a>
+    </p>
+    <p>
+      <a href="#results">Results</a>
+    </p>
+    <p>
+      <a href="#reviews">Client Reviews</a>
+    </p>
+  </>
+)
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+  return (
+    <div className="st__navbar">
+      <div className="st__navbar-links">
+        <div className="st__navbar-links_logo">
+          {/* <img src={logo} alt="logo" /> */}
+          <h4>Smart-Trading</h4>
+        </div>
+        <div className="st__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="st__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="st__navbar-menu">
+        {toggleMenu ? (
+          <CgMenuMotion
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <CgMenu color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        )}
+        {toggleMenu && (
+          <div className="st__navbar-menu_container shadow-drop-2-center">
+            <div className="st__navbar-menu_container-links">
+              <Menu />
+            </div>
+            <div className="st__navbar-menu_container-links-sign">
+              <p>Sign In</p>
+              <button type="button">Sign Up</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
